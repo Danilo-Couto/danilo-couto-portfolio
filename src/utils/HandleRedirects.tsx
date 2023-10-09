3c8c34be0c5317c626331eb1002a101a4a2b47a2
Danilo Couto <danilocoutodev@gmail.com>
2023-10-09 20:01:45 -0300

    feat: handleClickEmail

--- src/utils/HandleRedirects.tsx
+++ src/utils/HandleRedirects.tsx
@@ -1,3 +1,8 @@
 export const handleRedirect = (url: string) => {
   window.open(url);
 };
+
+export const handleEmailClick = () => {
+  window.location.href = `mailto:${'danilocoutodev@gmail.com'}`;
+};
+
