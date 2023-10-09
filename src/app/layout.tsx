bb45f79f36d749ec87cab9d19409f0068b442538
Danilo Couto <danilocoutodev@gmail.com>
2023-10-09 19:59:41 -0300

    feat: add LanguageProvider and ThemeModeContext at Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { LanguageProvider } from '../context/language-context'
+import { ThemeModeContext } from '@/context/thememode-context'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,10 +15,15 @@ export default function RootLayout({children}: {
   children: React.ReactNode
 }) {
 
-
   return (
     <html lang="pt">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <LanguageProvider>
+          <ThemeModeContext>
+           {children}
+          </ThemeModeContext>
+        </LanguageProvider>
+      </body>
     </html>
   )
 }
