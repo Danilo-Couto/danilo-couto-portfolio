892135de8ece19eda99c3ef96dfed4a9719e4e30
Danilo Couto <danilocoutodev@gmail.com>
2023-10-16 21:54:28 -0300

    refactor: theme context

--- src/app/theme-provider.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
- 
-import { createContext } from 'react'
- 
-export const ThemeContext = createContext({})
- 
-export default function ThemeProvider({ children }: any) {
-  return <ThemeContext.Provider value="dark">{children}</ThemeContext.Provider>
-}
