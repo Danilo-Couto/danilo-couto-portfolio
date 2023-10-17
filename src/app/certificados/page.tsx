f964f7756737a94cbf6c988e6bc502c961263c9c
Danilo Couto <danilocoutodev@gmail.com>
2023-10-17 18:30:30 -0300

    feat(certificados): add component to page

---
+++ src/app/certificados/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+import { SubCertificados } from "@/components/sub_certificados";
+
+export default function Certificados() {
+  return (
+    <main>
+      <Header/>
+      <SubCertificados/>
+      <Footer/>
+    </main>
+  );
+}
